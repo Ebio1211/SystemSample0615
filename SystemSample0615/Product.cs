f6b3669b2c4249d04f7558e9bd60754dbf488e72
Takashi Arai <31059@ojs.ac.jp>
2020-10-26 13:18:14 +0900

    プロジェクト ファイルを追加します。

---
+++ SystemSample0615/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemSample0615
+{
+    //商品クラス
+    class Product
+    {
+        //商品コード
+        public int Code { get; set; } //自動実装プロパティ
+        //商品名
+        public string Name { get; set; }
+        //商品価格
+        private int Price { get; set; }
+
+        //消費税の価格を求める
+        public int GetTax()
+        {
+            return(int)( Price * 0.1);
+        }
+
+        //税込み価格を求める
+        public int GetPriceIncludingTax()
+        {
+            return Price + GetTax();
+        }
+
+        //コンストラクタ
+        public Product(int code,string name,int price)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Price = price;
+        }
+
+
+    }
+}
---
+++ SystemSample0615/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemSample0615
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] products = new Product[5];
+
+            products[0] = new Product(123, "かりんとう", 180);
+            products[1] = new Product(124, "枝豆", 130);
+            products[2] = new Product(125, "プリン", 210);
+            products[3] = new Product(126, "パフェ", 450);
+            products[4] = new Product(127, "焼き鳥", 160);
+
+            //商品名と税込み価格を表示する
+            //かりんとう：198円
+            /*Console.WriteLine("{0}:{1}円",karinto.Name , karinto.GetPriceIncludingTax());
+            Console.WriteLine("{0}:{1}円", edamame.Name, edamame.GetPriceIncludingTax());
+            Console.WriteLine("{0}:{1}円", purin.Name, purin.GetPriceIncludingTax());
+            Console.WriteLine("{0}:{1}円", pafe.Name, pafe.GetPriceIncludingTax());
+            Console.WriteLine("{0}:{1}円", yakitori.Name, yakitori.GetPriceIncludingTax());
+            */
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine("{0}:{1}円", products[i].Name, products[i].GetPriceIncludingTax());
+            }
+
+
+        }
+    }
+}
